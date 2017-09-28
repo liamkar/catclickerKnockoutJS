66e84a672c673bc1f5a1f51c31e29c2634271e76
Ville <villekarjala@gmail.com>
2017-09-28 23:27:49 +0300

    list of cats added

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,8 +1,49 @@
-let Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('images/cat1.jpg');
-  this.imgAttribution = ko.observable('https://www.flickr.com');
+let initialCats = [
+{
+	clickCount : 0,
+	name: 'Pekka',
+	imgSrc: 'images/cat1.jpg',
+	imgAttribution : 'https://flickr.com',
+	nicknames: ['peksi','pera']
+},
+{
+	clickCount : 0,
+	name: 'Rontti',
+	imgSrc: 'images/cat2.jpg',
+	imgAttribution : 'https://flickr.com',
+	nicknames: ['ron','ronni']
+},
+{
+	clickCount :0,
+	name: 'Matti',
+	imgSrc: 'images/cat3.jpg',
+	imgAttribution : 'https://flickr.com',
+	nicknames: ['matmat','mamam']
+},
+{
+	clickCount :0,
+	name: 'Catt',
+	imgSrc: 'images/cat4.jpg',
+	imgAttribution : 'https://flickr.com',
+	nicknames: ['cathy','cattye']
+},
+{
+	clickCount :0,
+	name: 'Purry',
+	imgSrc: 'images/cat5.jpg',
+	imgAttribution : 'https://flickr.com',
+	nicknames: ['pu pu','prprpr']
+}
+];
+
+
+
+let Cat = function(data) {
+  //TODO:throws this.clickcount not a method error if no ko.observable defined.
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = data.name;
+  this.imgSrc = data.imgSrc;
+  this.imgAttribution = data.imgAttribution;
   this.level = ko.computed(function() {
     let clickCount = this.clickCount();
     if (clickCount < 10) {
@@ -14,6 +55,28 @@ let Cat = function() {
     }
   }, this);
 
+  this.nicknames = ko.observableArray(data.nicknames);
+
+
+  /*
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.level = ko.computed(function() {
+    let clickCount = this.clickCount();
+    if (clickCount < 10) {
+      return "NewBorn";
+    } else if (clickCount >= 10 && clickCount < 100) {
+      return "Infant";
+    } else if (clickCount >= 100) {
+      return "Teen";
+    }
+  }, this);
+
+  this.nicknames = ko.observableArray(data.nicknames);
+*/
+/*
   this.nicknames = ko.observableArray([{
       nick: 'oboy'
     },
@@ -24,11 +87,21 @@ let Cat = function() {
       nick: 'meanymai'
     }
   ]);
+  */
 };
 
 
 var ViewModel = function() {
-  this.currentCat = ko.observable(new Cat());
+  self = this;
+
+this.catList = ko.observableArray([]);
+initialCats.forEach(function(catItem) {
+  console.log(catItem);
+  self.catList.push(new Cat(catItem));
+});
+
+  this.currentCat = ko.observable(this.catList()[0]);
+
   this.incrementCounter = function() {
     /*
     console.log(this);
@@ -36,8 +109,26 @@ var ViewModel = function() {
     console.log(this.currentCat());
     this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     */
-    this.clickCount(this.clickCount() + 1);
+
+    console.log("self clickcount:"+self.currentCat().clickCount());
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   }
+
+
+//this.changeCurrentCat = function() {
+this.changeCurrentCat = function(item, event) {
+    console.log('change current cat');
+    console.log($(event.target));
+    console.log($(event.target.id));
+    console.log(item);
+    //this.currentCat = item;
+    //self.currentCat = item;
+    //this.currentCat = ko.observable(this.catList()[0]);
+    //self.currentCat = ko.observable(self.catList()[4]);
+    self.currentCat(item);
+    //self.currentCat = self.catList()[4];
+  }
+
 
 }
 
